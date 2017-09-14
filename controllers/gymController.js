dcabb768b4ad83838641c9d403d14cbdaa0ccf42
LyraXaph <jelena.brajnovicv@gmail.com>
2017-09-14 23:36:13 +0200

    store gym in db

--- controllers/gymController.js
+++ controllers/gymController.js
@@ -1,7 +1,17 @@
+const mongoose = require('mongoose');
+const Gym = mongoose.model('Gym');
+
+
 exports.homePage = (req, res) => {
     res.render('index', { title: "homePage"});
-}
+};
 
 exports.addGym = (req, res) => {
     res.render('editGym', {title: 'Add Gym'});
-}
+};
+
+exports.createGym = async (req, res) => {
+    const gym = new Gym(req.body);
+    await gym.save();
+    res.redirect('/');
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const gymController = require('../controllers/gymController')
+const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', gymController.homePage);
 router.get('/add', gymController.addGym);
+router.post('/add', catchErrors(gymController.createGym));
 
 module.exports = router;
