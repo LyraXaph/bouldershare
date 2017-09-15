05a89aaee6a7572fdab8dac1412e8e9fdcaaea5a
Jelena JB. Brajnović <brajnovic@OFFICE.OPTIMIT.HR>
2017-09-15 15:31:58 +0200

    create user accounts

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gymController = require('../controllers/gymController')
+const userController = require('../controllers/userController')
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', gymController.homePage);
@@ -9,6 +10,14 @@ router.get('/add', gymController.addGym);
 router.post('/add', catchErrors(gymController.createGym));
 router.post('/add/:id', catchErrors(gymController.updateGym));
 router.get('/gyms/:id/edit', catchErrors(gymController.editGym));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+//1. Validate registration data
+//2. register the user
+// 3. log them in
+router.post('/register', userController.validateRegister )
 
 module.exports = router;
 
